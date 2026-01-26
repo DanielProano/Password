643d42bd65eb0e763b4ba95603003e104bd1c955
Danny <179722984+DanielProano@users.noreply.github.com>
2026-01-26 18:41:41 -0500

    Adjusted base URL address

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import config from "./src/config.json";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/pass/",
   server: {
     host: "0.0.0.0",
     port: parseInt(config.port) || 5173,
