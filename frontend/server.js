f242a65cb8c761e70bf791c48ecdbde6d749f454
Danny <179722984+DanielProano@users.noreply.github.com>
2026-01-26 19:36:11 -0500

    Updated routes

--- frontend/server.js
+++ frontend/server.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 5173;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use("/pass/", express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use("/pass/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 import config from "./src/config.json";
 
 export default defineConfig({
+  base: "/pass/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
