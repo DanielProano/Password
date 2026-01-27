d70cb05cc0717dcc22b64ca9cb1c52a930e7c251
Danny <179722984+DanielProano@users.noreply.github.com>
2026-01-26 20:53:57 -0500

    another attempt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 import config from "./src/config.json";
 
 export default defineConfig({
-  base: "/pass/",
+  base: "https://dannyproano.com/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
